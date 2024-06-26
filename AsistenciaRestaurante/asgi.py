381057af9010730500a1acb37fdbcda32ec75d35
PracticanteT <sales@prointimo.co>
2024-06-26 06:56:37 -0500

    creacion del proyecto

---
+++ AsistenciaRestaurante/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AsistenciaRestaurante project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AsistenciaRestaurante.settings')
+
+application = get_asgi_application()
