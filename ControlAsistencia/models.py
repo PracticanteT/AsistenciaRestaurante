c0cf2f6e77f6d4cad22dff4d8a02b17bbde7d3b3
PracticanteT <sales@prointimo.co>
2024-06-26 08:59:03 -0500

    formulario funcional con lector de codigo de barras

--- ControlAsistencia/models.py
+++ ControlAsistencia/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Empleado(models.Model):
-    codigo = models.CharField(max_length=10, unique=True)
+    codigo = models.CharField(max_length=50, unique=True)
     nombre = models.CharField(max_length=100)
     genero = models.CharField(max_length=25, choices=[
         ('masculino', 'MASCULINO'),
