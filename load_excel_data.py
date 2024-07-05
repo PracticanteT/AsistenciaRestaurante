def08dbbc5e61963d2797b56f62e64d9b4fbc258
PracticanteT <sales@prointimo.co>
2024-07-05 12:56:15 -0500

    actualizacion formato cedula

--- load_excel_data.py
+++ load_excel_data.py
@@ -11,7 +11,7 @@ from ControlAsistencia.models import Empleado
 
 def load_excel_data(file_path):
     # Leer el archivo Excel tratando la columna de códigos como texto
-    df = pd.read_excel(file_path, dtype={'CODIGO': str})  # Especifica que la columna 'CODIGO' debe ser tratada como texto
+    df = pd.read_excel(file_path, dtype={'CODIGO': str, 'NUMCEDULA': str})  # Especifica que la columna 'CODIGO' debe ser tratada como texto
 
     # Iterar a través de las filas del DataFrame e insertar los datos en la base de datos
     for index, row in df.iterrows():
